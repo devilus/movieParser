9893dd86978125597db54fd43c668b0ff5293988
Max Lyubimov <maxlubimov@gmail.com>
2022-05-08 17:11:43 +0300

    Add express

--- cron.js
+++ cron.js
@@ -1,6 +1,8 @@
+import express from 'express';
 import nodeCron from 'node-cron';
 import { parse } from './src/parse.js';
 
+const port = process.env.PORT || 3000;
 const h = process.env.RUN_AT_HOUR || 12;
 const m = process.env.RUN_AT_MINUTE || 0;
 
@@ -9,3 +11,5 @@ const updateTask = nodeCron.schedule(`${m} ${h} * * *`, () => {
 });
 
 updateTask.start();
+
+express().listen(port, () => console.log(`Listening on ${port}`));
