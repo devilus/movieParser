f3f36a843ba0f4891bcb78c4128bf16f79e62a52
Max Lyubimov <maxlubimov@gmail.com>
2022-05-08 10:38:58 +0300

    Cron config

--- cron.js
+++ cron.js
@@ -1,7 +1,10 @@
 import nodeCron from 'node-cron';
 import { parse } from './src/parse.js';
 
-const updateTask = nodeCron.schedule('30 10 * * *', () => {
+const h = process.env.RUN_AT_HOUR || 12;
+const m = process.env.RUN_AT_MINUTE || 0;
+
+const updateTask = nodeCron.schedule(`${m} ${h} * * *`, () => {
   parse();
 });
 
