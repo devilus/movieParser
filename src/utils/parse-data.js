65d742fe987040ed3c516cc4777e56802474f9cd
Max Lyubimov <maxlubimov@gmail.com>
2022-05-23 18:37:08 +0300

    Fix axios errors catch

--- src/utils/parse-data.js
+++ src/utils/parse-data.js
@@ -1,54 +1,63 @@
 import _ from 'lodash';
-import { axiosInstance } from '../../settings/axios.js';
+import { axiosClient } from '../../settings/axios.js';
 import { stepTimeout } from './step-timeout.js';
 import { saveData } from './save-data.js';
 
+const chunkSize = 10;
+
+const makeRequestPromise = (movieID, index) =>
+  new Promise((resolve, reject) => {
+    stepTimeout(() => {
+      axiosClient
+        .get(`${movieID}`)
+        .then((response) => {
+          const movieData = response.data.item;
+          resolve(movieData);
+        })
+        .catch(() => {
+          reject(new Error('Parsing error'));
+        });
+    }, index);
+  });
+
 export const parseData = (...movieIDs) => {
-  console.log(`Data parsing of ${movieIDs.length} movies...`);
-
-  const chunkSize = 10;
   const chunks = _.chunk(movieIDs, chunkSize);
-
   const getChunk = (chunkNum) => chunks[chunkNum] ?? null;
 
-  const makeRangeString = (chunkNum) => {
+  const makeRangeText = (chunkNum) => {
     const { length } = getChunk(chunkNum);
     const chunkEnd = length < chunkSize ? length : (chunkNum + 1) * chunkSize;
     const chunkStart = chunkEnd - length + 1;
 
-    return `[${chunkStart}-${chunkEnd}]`;
+    return `${chunkNum + 1} - [${chunkStart}-${chunkEnd}]`;
   };
 
   const parseChunk = async (chunkNum) => {
     const chunk = getChunk(chunkNum);
-
     if (!chunk) {
       console.log('Parsing completed');
       return;
     }
 
     // Build promises collection
-    const promises = chunk.map(
-      (movieID, index) =>
-        new Promise((resolve, reject) => {
-          stepTimeout(async () => {
-            const response = await axiosInstance.get(`${movieID}`);
-            const movieData = response.data.item;
-
-            resolve(movieData);
-            reject(new Error('Parsing error'));
-          }, index);
-        })
+    const promises = chunk.map((movieID, index) =>
+      makeRequestPromise(movieID, index)
     );
 
     // Save to db
     try {
       const data = await Promise.all(promises);
       await saveData(...data);
-      console.log(`Parsed data chunk: ${makeRangeString(chunkNum)}`);
+
+      console.log(`Data chunk ${makeRangeText(chunkNum)} parsed successfully`);
       parseChunk(chunkNum + 1); // Run with the next data chunk
     } catch (error) {
-      console.log(error);
+      // console.log(error);
+      console.log(
+        `Something went wrong when parsing chunk ${makeRangeText(
+          chunkNum
+        )}. Trying to parse again...`
+      );
       setTimeout(() => {
         parseChunk(chunkNum); // Rerun with the same data chunk
       }, 60000);
--- src/utils/parse-ids.js
+++ src/utils/parse-ids.js
@@ -1,5 +1,5 @@
 import _ from 'lodash';
-import { axiosInstance } from '../../settings/axios.js';
+import { axiosClient } from '../../settings/axios.js';
 import { stepTimeout } from './step-timeout.js';
 import { Movie } from '../../settings/db.js';
 
@@ -9,46 +9,40 @@ const params = {
 };
 
 const getPagesCount = async () => {
-  const response = await axiosInstance.get('', { params });
+  const response = await axiosClient.get('', { params });
   return response.data.pagination.total;
 };
 
-export const parseIDs = async (options = { forceUpdate: false }) => {
-  console.log('IDs parsing...');
+const makeRequestPromise = (pageNum) =>
+  new Promise((resolve, reject) => {
+    stepTimeout(
+      () => {
+        axiosClient
+          .get('', {
+            params: { ...params, page: pageNum },
+          })
+          .then((response) => {
+            const { items } = response.data;
+            const movieIDs = items.map((item) => item.id);
+            resolve(movieIDs);
+          })
+          .catch(() => {
+            reject(new Error('Parsing error'));
+          });
+      },
+      pageNum,
+      { min: 20, max: 50 }
+    );
+  });
 
+export const parseIDs = async (options = { forceUpdate: false }) => {
   const pagesCount = await getPagesCount();
   const pages = _.range(1, pagesCount + 1);
 
   // Build promises collection
-  const promises = pages.map(
-    (pageNum) =>
-      new Promise((resolve, reject) => {
-        stepTimeout(
-          async () => {
-            const response = await axiosInstance.get('', {
-              params: { ...params, page: pageNum },
-            });
-            const { items } = response.data;
-            const movieIDs = items.map((item) => item.id);
+  const promises = pages.map((pageNum) => makeRequestPromise(pageNum));
 
-            resolve(movieIDs);
-            reject(new Error('Parsing error'));
-          },
-          pageNum,
-          { min: 20, max: 50 }
-        );
-      })
-  );
-
-  let movieIDs = [];
-  try {
-    movieIDs = await Promise.all(promises).then(_.flatten);
-  } catch (error) {
-    console.log(error);
-    setTimeout(() => {
-      parseIDs(options);
-    }, 60000);
-  }
+  const movieIDs = await Promise.all(promises).then(_.flatten);
 
   // Return all
   if (options.forceUpdate) {
